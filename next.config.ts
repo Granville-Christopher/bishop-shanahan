a61058deed0f8711f9ef4c16d9e7dfe6482a92c0
Granville christopher <granvillechristopher@outlook.com>
2026-05-18 11:50:36 -0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
+  allowedDevOrigins: ['192.168.10.73'],
 }
 
 export default nextConfig;
